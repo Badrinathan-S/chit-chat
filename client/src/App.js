8dbc4ebb04c9b31121879ef1bb2fadc865025cce
Badrinathan-S <badrinathan3@gmail.com>
2022-12-09 15:53:34 +0530

    structure defined!!

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Button } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Peter!</h1>
+        <Button colorScheme='blue'>Button</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const {chats} = require("./data/data")
+const dotenv = require("dotenv");
+
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+
+    res.send("api is running!")
+
+})
+
+app.get("/api/chats", (req, res) => {
+
+    res.send(chats);
+
+})
+
+app.get("/api/chat/:id", (req, res) => {
+
+    // console.log(req.params.id);
+
+    let singleChat = chats.find((chat) => req.params.id === chat._id );
+    console.log(singleChat);
+    res.send(singleChat);
+
+})
+
+app.listen(PORT, console.log("server started"))
