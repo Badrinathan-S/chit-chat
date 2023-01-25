baccabf37efe83f5cdcfe96035e9811cf582cd43
-Badrinathan-S <badrinathan3@gmail.com>
2023-01-25 23:07:05 +0530

    signup page completed

---
+++ client/src/component/pages/ChatsPage.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const ChatsPage = () => {
+  const [chats, setChats] = useState([]);
+
+  const fetchChats = async () => {
+    const { data } = await axios.get("/api/chats");
+    console.log(data);
+    setChats(data);
+  };
+
+  useEffect(() => {
+    fetchChats();
+  }, []);
+
+  return (
+    <div>
+      {chats.map((chat) => (
+        <h6 key={chat._id}>{chat.chatName} </h6>
+      ))}
+    </div>
+  );
+};
+
+export default ChatsPage;
