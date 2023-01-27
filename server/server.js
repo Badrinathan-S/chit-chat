3043151b6d6badab8367a198839454daffa02f7c
-Badrinathan-S <badrinathan3@gmail.com>
2023-01-27 23:01:16 +0530

    Mongo DB connection done

--- server/server.js
+++ server/server.js
@@ -1,32 +1,27 @@
 const express = require("express");
-const {chats} = require("./data/data")
+const { chats } = require("./data/data");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
-
-const app = express();
 dotenv.config();
+connectDB();
+const app = express();
 
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
-
-    res.send("api is running!")
-
-})
+  res.send("api is running!");
+});
 
 app.get("/api/chats", (req, res) => {
-
-    res.send(chats);
-
-})
+  res.send(chats);
+});
 
 app.get("/api/chat/:id", (req, res) => {
+  // console.log(req.params.id);
 
-    // console.log(req.params.id);
+  let singleChat = chats.find((chat) => req.params.id === chat._id);
+  console.log(singleChat);
+  res.send(singleChat);
+});
 
-    let singleChat = chats.find((chat) => req.params.id === chat._id );
-    console.log(singleChat);
-    res.send(singleChat);
-
-})
-
-app.listen(PORT, console.log("server started"))
+app.listen(PORT, console.log("server started"));
